4cd24adeb9d510bb4031ebd933136c0e22a1351b
Damirdb <115777005+Damirdb@users.noreply.github.com>
2022-12-15 19:21:15 +0500

    Update 6.c

--- 6.c
+++ 6.c
@@ -11,10 +11,10 @@ void fill(int n, int a[])
 
 int maximum(int n, int A[])
 {
-    int m, sum;
-    m = -120;
-    int k;
-    for (k = 0; k < n-1; k++)
+int m, sum;
+m = -120;
+int k;
+for (k = 0; k < n-1; k++)
     {
         sum = A[k] + A[k + 1];
         if (sum > m)
@@ -34,12 +34,12 @@ void GetArray(int n, int A[])
 
 int sort(int n, int A[])
 {
-    int i, j, min1, min2, result;
-    for(i = 0 ; i < n - 1; i++)
+int i, j, min1, min2, result;
+for(i = 0 ; i < n - 1; i++)
     {
-        for(j = 0 ; j < n - i - 1 ; j++)
+    for(j = 0 ; j < n - i - 1 ; j++)
         {
-            if (A[j] > A[j+1])
+        if (A[j] > A[j+1])
             {
                 int tmp = A[j];
                 A[j] = A[j+1] ;
@@ -47,23 +47,23 @@ int sort(int n, int A[])
             }
         }
     }
-    GetArray(n, A);
-    min1 = A[0]; min2 = A[1];
-    result = min1 + min2;
+GetArray(n, A);
+min1 = A[0]; min2 = A[1];
+result = min1 + min2;
     return result;
 }
 
 int main()
 {
-    srand(time(NULL));
-    int n, max, otvet, k;
-    printf("Введи кол-во элементов в массиве -> ");
-    scanf("%d", &n);
-    int A[n];
-    fill(n, A);
-    GetArray(n, A);
-    max = maximum(n, A);
-    k = sort(n, A);
-    otvet = max + k;
-    printf("Ответ: %d\n", otvet);
+srand(time(NULL));
+int n, max, otvet, k;
+printf("Введи кол-во элементов в массиве -> ");
+scanf("%d", &n);
+int A[n];
+fill(n, A);
+GetArray(n, A);
+max = maximum(n, A);
+k = sort(n, A);
+otvet = max + k;
+printf("Ответ: %d\n", otvet);
 }
